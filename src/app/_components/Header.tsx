ce7a631c82c8ac57c3caaf64c3709ceb53fcd8eb
bit1creativee <ivan.hrytsenko@nbcuni.com>
2024-03-03 14:06:11 +0200

    fix bg

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export const Header = () => (
   <header className="w-full mx-auto max-w-5xl 2xl:max-w-[1440px]">
-    <div className="py-4 text-white md:py-8 text-2xl md:text-3xl 2xl:text-4xl">
+    <div className="2xl:py-0 py-4 text-white md:py-8 text-2xl md:text-3xl 2xl:text-4xl">
       <Link href={"/"}>hobbyplace.</Link>
     </div>
   </header>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,32 +3,63 @@ import { InfoTabs } from "./_components/InfoTabs";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex justify-between items-center flex-col bg-home-page bg-cover bg-right md:px-14 2xl:bg-[-75%_50%] bg-no-repeat 2xl:bg-[#002333]">
-      <Header />
+    <>
+      <main className="2xl:hidden min-h-screen flex justify-between items-center flex-col bg-home-page bg-cover bg-right md:px-14 2xl:bg-[-450%_100%] bg-no-repeat 2xl:bg-[#002333]">
+        <Header />
 
-      <div className="mx-auto max-w-5xl 2xl:max-w-[1440px]">
-        <div className="2xl:hidden bg-white w-full py-8 px-6 text-custom-black md:py-10 md:px-8">
-          <div className="uppercase font-unbounded font-semibold text-lg md:text-4xl">
-            Платформа для пошуку, розвитку та вдосконалення своїх навичок
-          </div>
+        <div className="mx-auto max-w-5xl 2xl:max-w-[1440px]">
+          <div className="2xl:hidden bg-white w-full py-8 px-6 text-custom-black md:py-10 md:px-8">
+            <div className="uppercase font-unbounded font-semibold text-lg md:text-4xl">
+              Платформа для пошуку, розвитку та вдосконалення своїх навичок
+            </div>
 
-          <hr className="w-full mx-auto my-8 border-custom-gray" />
+            <hr className="w-full mx-auto my-8 border-custom-gray" />
 
-          <InfoTabs />
-        </div>
-
-        <div className="hidden 2xl:block w-1/2">
-          <div className="uppercase font-unbounded font-semibold text-lg md:text-4xl">
-            Платформа для пошуку, розвитку та вдосконалення своїх навичок
-          </div>
-
-          <div className="bg-white px-8 py-10 mt-10">
             <InfoTabs />
           </div>
-        </div>
-      </div>
 
-      <div></div>
-    </main>
+          <div className="hidden 2xl:block w-1/2">
+            <div className="uppercase font-unbounded font-semibold text-lg md:text-4xl">
+              Платформа для пошуку, розвитку та вдосконалення своїх навичок
+            </div>
+
+            <div className="bg-white px-8 py-10 mt-10">
+              <InfoTabs />
+            </div>
+          </div>
+        </div>
+
+        <div></div>
+      </main>
+
+      <main className="hidden min-h-screen 2xl:flex justify-between items-center flex-col bg-[#002333]">
+        <div></div>
+        <div className="mx-auto flex flex-row items-center justify-center pl-24">
+            <div className="w-1/2 flex flex-col justify-around h-full min-h-screen">
+
+              <div className="mb-24">
+              <Header />
+              </div>
+
+              <div>
+
+              <div className="uppercase font-unbounded font-semibold text-lg md:text-4xl">
+                Платформа для пошуку, розвитку та вдосконалення своїх навичок
+              </div>
+
+              <div className="bg-white px-8 py-10 mt-10">
+                <InfoTabs />
+              </div>
+
+              </div>
+
+              <div></div>
+          </div>
+          <div className="bg-home-page bg-cover w-full h-full min-h-screen"></div>
+        </div>
+
+        <div></div>
+      </main>
+    </>
   );
 }
