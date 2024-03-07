6ebebee2e87463e71f8e79ad861264ddd71c42ac
bit1creativee <ivan.hrytsenko@nbcuni.com>
2024-03-07 12:27:35 +0200

    text white on dark theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <Header />
 
         <div className="mx-auto max-w-5xl 2xl:max-w-[1440px]">
-          <div className="2xl:hidden bg-white w-full py-8 px-6 text-custom-black md:py-10 md:px-8">
+          <div className="bg-white w-full py-8 px-6 text-custom-black md:py-10 md:px-8">
             <div className="uppercase font-unbounded font-semibold text-lg md:text-4xl">
               Платформа для пошуку, розвитку та вдосконалення своїх навичок
             </div>
@@ -16,16 +16,6 @@ export default function Home() {
             <hr className="w-full mx-auto my-8 border-custom-gray" />
 
             <InfoTabs />
-          </div>
-
-          <div className="hidden 2xl:block w-1/2">
-            <div className="uppercase font-unbounded font-semibold text-lg md:text-4xl">
-              Платформа для пошуку, розвитку та вдосконалення своїх навичок
-            </div>
-
-            <div className="bg-white px-8 py-10 mt-10">
-              <InfoTabs />
-            </div>
           </div>
         </div>
 
@@ -43,7 +33,7 @@ export default function Home() {
 
               <div>
 
-              <div className="uppercase font-unbounded font-semibold text-lg md:text-4xl">
+              <div className="text-white uppercase font-unbounded font-semibold text-lg md:text-4xl">
                 Платформа для пошуку, розвитку та вдосконалення своїх навичок
               </div>
 
